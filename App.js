d3157be2863f69fbb0b6f938a88761a4edd3d3be
Bojan Jagetic <jagetic.bojan@gmail.com>
2022-09-26 12:43:00 +0200

    homescreen added and google autocomplete

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState , useEffect} from 'react'
+import {  SafeAreaView, StyleSheet, TextInput, Text, Keyboard, Alert,  Button,
+  useColorScheme } from 'react-native';
+import {Provider } from "react-redux";
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+      <SafeAreaProvider>
+        <Stack.Navigator>
+            <Stack.Screen 
+            name="HomeScreen"
+            component={HomeScreen}
+            options={{
+              headerShown: false
+            }}
+            />    
+            <Stack.Screen 
+            name="MapScreen"
+            component={MapScreen}
+            options={{
+              headerShown: false
+            }}
+            />
+        </Stack.Navigator>
+        {/* <HomeScreen/> */}
+      {/* <SafeAreaView style={styles.container}>
+      <Text> Building Kuber </Text>
+        </SafeAreaView> */}
+      </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex:1,
+    backgroundColor: '#e0e7ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    color: '#ffff',
+  },
+  text: {
+    color: '#cbd5e1',
+  }
+
 });
---
+++ screens/MapScreen.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import tw from "tailwind-react-native-classnames"
+import Map from '../components/Map'
+
+const MapScreen = () => {
+  return (
+    <View>
+      <Text>Here is map stuff </Text>
+        <View style={tw`h-1/2`}>
+            <Map />
+        </View>
+        <View style={tw`h-1/2`}></View>
+    </View>
+  )
+}
+
+export default MapScreen
+
+const styles = StyleSheet.create({})
